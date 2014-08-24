7b7fb4c73c81da78df3826cf59951cfe7f88e570
Jason Bryer <jason@bryer.org>
2014-08-24 19:37:30 -0400

    Added Setup.R

---
+++ R/Setup.R
@@ -0,0 +1,8 @@
+install.packages(c('openintro','OIdata','devtools','ggplot2','psych','reshape2'), 
+				 repos='http://cran.r-project.org')
+
+require(openintro)
+require(OIdata)
+
+data(package='openintro')
+data(package='OIdata')
